5d9a8048421e23398f30a9dfda9690129824fd1c
Artur Inoue <arturcorreainoue@gmail.com>
2024-11-13 12:31:36 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+Restaurant.create([
+  { name: "Sushi", address: "Tokyo", phone_number: "123-456-7890", category: "japanese" },
+  { name: "Pizza", address: "Rome", phone_number: "234-567-8901", category: "italian" },
+  { name: "Baguette", address: "Paris", phone_number: "345-678-9012", category: "french" },
+  { name: "Waffle", address: "Brussels", phone_number: "456-789-0123", category: "belgian" },
+  { name: "Chinese", address: "Beijing", phone_number: "567-890-1234", category: "chinese" }
+])
